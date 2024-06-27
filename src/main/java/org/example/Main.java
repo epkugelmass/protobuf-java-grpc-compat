072ea47dd88e7afdbe1b437ac133eb23285e37ef
Elan P. Kugelmass <epkugelmass@gmail.com>
2024-06-27 00:21:17 -0400

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import io.grpc.health.v1.HealthCheckRequest;
+
+public class Main {
+    public static void main(String[] args) {
+        HealthCheckRequest.newBuilder().build();
+    }
+}
